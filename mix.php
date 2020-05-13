95c4923c58aba7bb0b65f59fd44132a1161c7174
Dajan Vulaj <dajanvulaj@gmail.com>
2020-05-13 22:25:26 +0200

    Update: NPM Packages upgraded

--- mix.php
+++ mix.php
@@ -42,7 +42,6 @@ if (!function_exists('mix')) {
             $path = "/{$path}";
         }
 
-        // $path = $publicFolder . $path;
         if (!array_key_exists($path, $manifest)) {
             throw new Exception(
                 "Unable to locate Mix file: {$path}. Please check your " .
